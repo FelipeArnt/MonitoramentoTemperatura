734357f2f1d39be2cad1c9ae95648b4015f723d5
Felipe Arnt <102182726+FelipeArnt@users.noreply.github.com>
2025-04-08 13:13:04 -0300

    Update and rename TemperatureMonitor.ino to MonitorTemp.ino

--- MonitorTemp.ino
+++ MonitorTemp.ino
@@ -1,21 +1,21 @@
 #include <Arduino.h>  // Inclui a biblioteca principal do Arduino
 
-#define TEMP_THRESHOLD 30
+#define TEMP_LIMITE 30 // Definição do valor limite da temperatura
 #define LED_PIN 2
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);  // Configura o pino do LED como saída
-    Serial.begin(9600);  // Inicializa a comunicação serial
+    Serial.begin(9600);       // Inicializa a comunicação serial -> BaudRate
 }
 
 void loop() {
-    int sensorValue = analogRead(A0);  // Lê o valor do sensor no pino A0
-    float temperature = (sensorValue * 5.0 / 1024.0) * 100.0;  // Converte para temperatura
+    int valorSensor = analogRead(A0);  // Lê o valor do sensor no pino A0
+    float temperatura = (valorSensor * 5.0 / 1024.0) * 100.0;  // Converte para temperatura
 
     Serial.print("Temperatura: ");
-    Serial.println(temperature);
+    Serial.println(temperatura);
 
-    if (temperature > TEMP_THRESHOLD) {
+    if (temperatura > TEMP_LIMITE) {
         digitalWrite(LED_PIN, HIGH);  // Liga o LED
     } else {
         digitalWrite(LED_PIN, LOW);  // Desliga o LED
